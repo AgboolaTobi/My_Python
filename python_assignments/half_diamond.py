1a34f54d9ed66474cbeba08d9d236c0ee24a5c4f
Agboola Tobi Samuel <tobi4tee@gmail.com>
2023-09-24 21:02:13 +0100

    Made this commit on saturday after I refractor my half_diamond assignment on sunday24/09/23

--- python_assignments/half_diamond.py
+++ python_assignments/half_diamond.py
@@ -2,8 +2,7 @@ for i in range(0, 5):
     for j in range(i+1):
         print("* ", end="")
     print()
-for i in range(i + 1, 0, -1):
-    for j in range(0, i - 1):
-        print("*", end=' ')
-    print("")
-
+for i in range(0, 5):
+    for j in range(i+1, 5):
+        print("*", end=" ")
+    print()
