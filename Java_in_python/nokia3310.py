b61cbf3d9944f9792d6fffd27f837b7f5817c22c
Agboola Tobi Samuel <tobi4tee@gmail.com>
2023-10-02 22:39:33 +0100

    Made this commit on Monday 02/10/23

---
+++ Java_in_python/nokia3310.py
@@ -0,0 +1,16 @@
+print("""
+ ===List of Menu Functions===
+                1. Phone book
+                2. Message
+                3. Chat
+                4. Call register
+                5. Tones
+                6. Settings
+                7. Call divert
+                8. Games
+                9. Calculator
+                10. Reminders
+                11. Clock
+                12. Profiles
+                13. SIM Services
+""")
