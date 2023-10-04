7dee72a5e7b4725431735a1e8bab82fb5ccfcc51
Ayinlade Sikiru <ayinladesikiru@gmail.com>
2023-10-04 19:32:53 +0100

    Update multiple.py

--- python_assignments/roasted_corn/multiple.py
+++ python_assignments/roasted_corn/multiple.py
@@ -3,4 +3,5 @@ result_of_multiple = []
 for element in numbers:
     result_of_multiple.append(element*2)
 print(result_of_multiple)
-    
+
+# because of the mutability nature of list, you dont need another list to add your result
